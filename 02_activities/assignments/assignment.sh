57e7483b16d7f30d679f0b97ee2007785ba47fe4
Darling <darling.oscanoa@gmail.com>
2025-04-13 03:50:01 -0400

    solving the step 7 not deleting all files that contains ippaddr

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -46,8 +46,8 @@ cp ./data/raw/user*.log ./data/processed/user_logs/
 cp ./data/raw/event*.log ./data/processed/event_logs/
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-rm -rf ./data/raw/user_ipaddr_*.log
-rm -rf ./data/processed/user_logs/user_ipaddr_*.log
+rm -rf ./data/raw/*ipaddr*
+rm -rf ./data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 ls -ls ./data/processed/server_logs/ > ./data/inventory.txt
